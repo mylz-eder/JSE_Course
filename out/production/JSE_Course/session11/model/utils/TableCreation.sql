b50bfce9af43015252fc2ab08bc2270d656c35f8
Mylz <mylzederbusiness@gmail.com>
2024-08-18 19:42:31 +0330

    cleaned up and made the TableCreation.sql more readable

--- out/production/JSE_Course/session11/model/utils/TableCreation.sql
+++ out/production/JSE_Course/session11/model/utils/TableCreation.sql
@@ -1,4 +1,22 @@
-
+-- this is the process of starting a database that this program can interact with
+--
+--     Enter Username: sys as sysdba
+--     Enter Password:
+--
+--     ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+--     CREATE USER INTERNALLOGIN IDENTIFIED BY INTERNALLOGIN1234;
+--     GRANT DBA TO INTERNALLOGIN;
+--     QUIT
+--
+--     TERMINAL: SQL-PLUS
+--
+--     Enter Username: INTERNALLOGIN
+--     Enter Password: INTERNALLOGIN1234
+--
+-- Connected to:
+-- Oracle Database 21c Express Edition Release 21.0.0.0.0 - Production
+-- Version 21.3.0.0.0
+--
 -- CREATE TABLE PRODUCT (
 -- ID NUMBER(10) PRIMARY KEY,
 -- NAME NVARCHAR2(20),
